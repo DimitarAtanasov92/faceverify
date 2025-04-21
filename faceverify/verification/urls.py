fa37873b7fd48e14c9eae63f7496c516b0bbb0cd
DimitarAtanasov92 <141042344+DimitarAtanasov92@users.noreply.github.com>
2025-04-21 23:50:41 +0300

    Delete faceverify directory

--- faceverify/verification/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import register, verify_user, upload_image, verification_success
-
-urlpatterns = [
-    path('', register, name='register'),
-    path('verify/', verify_user, name='verify_user'),
-    path('upload/', upload_image, name='upload_image'),
-    path('success/', verification_success, name='verification_success'),
-]
